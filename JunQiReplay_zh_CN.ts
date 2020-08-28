9716373514fb4fc81b8a7f9d015e084466f530cb
tootal <tootal@yeah.net>
2020-08-28 13:35:03 +0800

    translate

--- JunQiReplay_zh_CN.ts
+++ JunQiReplay_zh_CN.ts
@@ -1,3 +1,85 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="JunQiReplay_zh_CN"></TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>JunQiReplay</source>
+        <translation>军旗复盘</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>Folder Path</source>
+        <translation>文件夹路径</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>Filter Name</source>
+        <translation>玩家名过滤</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <source>&amp;File</source>
+        <translation>文件(&amp;F)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="98"/>
+        <source>&amp;Open Folder</source>
+        <translation>打开文件夹(&amp;O)</translation>
+    </message>
+</context>
+<context>
+    <name>Model</name>
+    <message>
+        <location filename="model.cpp" line="86"/>
+        <source>Win</source>
+        <translation>胜利</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="87"/>
+        <source>Tie</source>
+        <translation>和局</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="88"/>
+        <source>Lose</source>
+        <translation>失败</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="122"/>
+        <source>Time</source>
+        <translation>时间</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="124"/>
+        <source>This</source>
+        <translation>本家</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="126"/>
+        <source>That</source>
+        <translation>对家</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="128"/>
+        <source>Previous</source>
+        <translation>上家</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="130"/>
+        <source>Next</source>
+        <translation>下家</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="132"/>
+        <source>Steps</source>
+        <translation>步数</translation>
+    </message>
+    <message>
+        <location filename="model.cpp" line="134"/>
+        <source>Result</source>
+        <translation>结果</translation>
+    </message>
+</context>
+</TS>
--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QTranslator>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setApplicationName("JunQiReplay");
     a.setOrganizationName("tootal");
+    QString locale = QLocale::system().name();
+    QLocale::setDefault(QLocale(locale));
+    QTranslator translator;
+    translator.load("JunQiReplay_" + locale, ":/");
+    a.installTranslator(&translator);
     MainWindow w;
     w.show();
     return a.exec();

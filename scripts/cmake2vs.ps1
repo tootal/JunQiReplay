f619fec8276d1770f69ec35509b8377777b5cc60
tootal <tootal@yeah.net>
2022-01-09 16:19:14 +0800

    指定图标

--- scripts/cmake2vs.ps1
+++ scripts/cmake2vs.ps1
@@ -1,8 +1,12 @@
 $FILE_DIR = Split-Path -Parent ${MyInvocation}.MyCommand.Definition
 $WORK_DIR = Split-Path -Parent ${FILE_DIR}
 Push-Location -Path ${WORK_DIR}
-$QT_PATH = 'C:\Qt\6.2.2\msvc2019_64'
-$BUILD_DIR = 'build/vs'
+
+Invoke-Expression -Command 'scripts\win_env.ps1'
+
+$BUILD_DIR = 'build/vs_build'
 cmake -DCMAKE_PREFIX_PATH="${QT_PATH}" -B "${BUILD_DIR}" -G "Visual Studio 16 2019" -T host=x64 -A x64
-cmake --open "${BUILD_DIR}"
+if (${LASTEXITCODE} -eq 0) {
+    # cmake --open "${BUILD_DIR}"
+}
 Pop-Location

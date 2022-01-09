60e2a14061c5d56da29f86063994c81beccaf048
tootal <tootal@yeah.net>
2022-01-09 14:34:37 +0800

    完善翻译

--- res/JunQiReplay_zh_CN.ts
+++ res/JunQiReplay_zh_CN.ts
@@ -4,82 +4,155 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="14"/>
+        <location filename="../src/mainwindow.ui" line="14"/>
         <source>JunQiReplay</source>
         <translation>军旗复盘</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="45"/>
+        <location filename="../src/mainwindow.ui" line="45"/>
         <source>Folder Path</source>
         <translation>文件夹路径</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="63"/>
+        <location filename="../src/mainwindow.ui" line="63"/>
         <source>Filter Name</source>
         <translation>玩家名过滤</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="104"/>
+        <location filename="../src/mainwindow.ui" line="104"/>
         <source>&amp;File</source>
         <translation>文件(&amp;F)</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="113"/>
+        <location filename="../src/mainwindow.ui" line="113"/>
         <source>&amp;Open Folder</source>
         <translation>打开文件夹(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="73"/>
+        <source>Parsing files...</source>
+        <translation>正在解析文件……</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="74"/>
+        <source>Abort</source>
+        <translation>中止</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="134"/>
+        <source>Time</source>
+        <translation>时间</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="135"/>
+        <source>This</source>
+        <translation>本家</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="136"/>
+        <source>That</source>
+        <translation>对家</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="137"/>
+        <source>Previous</source>
+        <translation>上家</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="138"/>
+        <source>Next</source>
+        <translation>下家</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="139"/>
+        <source>Steps</source>
+        <translation>步数</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="140"/>
+        <source>Result</source>
+        <translation>结果</translation>
     </message>
 </context>
 <context>
     <name>Model</name>
     <message>
-        <location filename="model.cpp" line="47"/>
+        <location filename="../src/model.cpp" line="52"/>
         <source>Win</source>
         <translation>胜利</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="48"/>
+        <location filename="../src/model.cpp" line="53"/>
         <source>Tie</source>
         <translation>和局</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="49"/>
+        <location filename="../src/model.cpp" line="54"/>
         <source>Lose</source>
         <translation>失败</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="85"/>
+        <location filename="../src/model.cpp" line="84"/>
         <source>Time</source>
         <translation>时间</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="87"/>
+        <location filename="../src/model.cpp" line="86"/>
         <source>This</source>
         <translation>本家</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="89"/>
+        <location filename="../src/model.cpp" line="88"/>
         <source>That</source>
         <translation>对家</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="91"/>
+        <location filename="../src/model.cpp" line="90"/>
         <source>Previous</source>
         <translation>上家</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="93"/>
+        <location filename="../src/model.cpp" line="92"/>
         <source>Next</source>
         <translation>下家</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="95"/>
+        <location filename="../src/model.cpp" line="94"/>
         <source>Steps</source>
         <translation>步数</translation>
     </message>
     <message>
-        <location filename="model.cpp" line="97"/>
+        <location filename="../src/model.cpp" line="96"/>
         <source>Result</source>
         <translation>结果</translation>
+    </message>
+    <message>
+        <location filename="../src/model.cpp" line="117"/>
+        <source>Parsing files...</source>
+        <translation>正在解析文件……</translation>
+    </message>
+    <message>
+        <location filename="../src/model.cpp" line="118"/>
+        <source>Abort</source>
+        <translation>中止</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/replay.cpp" line="217"/>
+        <source>Win</source>
+        <translation>胜利</translation>
+    </message>
+    <message>
+        <location filename="../src/replay.cpp" line="218"/>
+        <source>Tie</source>
+        <translation>和局</translation>
+    </message>
+    <message>
+        <location filename="../src/replay.cpp" line="219"/>
+        <source>Lose</source>
+        <translation>失败</translation>
     </message>
 </context>
 </TS>
